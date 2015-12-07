0e0466d64729ce55a10a174117f442a980bd2f36
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-12-07 13:12:29 +1100

    update to picture tile

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,18 +5,11 @@ import styles from './Tile.css';
 
 class Tile extends Component {
 
-  constructor (props, context) {
-    super(props, context);
-    this.state = {};
-  }
-
   render() {
 
     return (
-      <div className={styles.tile}>
-          <div className={styles.content}>
-            {this.props.children}
-          </div>
+      <div styleName='tile'>
+        {this.props.children}
       </div>
     );
   }
